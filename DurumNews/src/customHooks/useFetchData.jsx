627372234c9ca17d33663842853f80e24fd6b8e6
RobbLloyd90 <robbmorgan10@gmail.com>
2025-02-21 16:52:29 +0000

    should be able to like and unlike articles, should be able to submit a new comment

--- DurumNews/src/customHooks/useFetchData.jsx
+++ DurumNews/src/customHooks/useFetchData.jsx
@@ -4,16 +4,27 @@ import axios from "axios";
 const useFetchData = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
   useEffect(() => {
+    const abortCont = new AbortController();
     axios
-      .get(url)
+      .get(url, { signal: abortCont.signal })
       .then(({ data }) => {
         setData(data);
+        setIsPending(false);
+        setError(null);
       })
-      .then(() => {})
       .catch((error) => {
-        setError(error.message);
+        if (error.name === "AbortError") {
+          console.log("Get aborted");
+        } else {
+          setIsPending(false);
+          setError(error.message);
+        }
       });
+
+    return () => abortCont.abort();
   }, [url]);
   return { data, error };
 };
